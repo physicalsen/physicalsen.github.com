b69110b1f3acabde1f441054added6004c1d8f96
unknown <physicalsen>
2020-10-21 22:09:50 +0800

    add

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,3 @@
-let myHeading = document.querySelector('li');
-myHeading.textContent = myHeading.textContent + 'hello world!';
-
-let myImage = document.querySelector('img');
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/Bubble nebula.jpg') {
-      myImage.setAttribute('src', 'images/Galaxy1.jpg');
-    } else {
-      myImage.setAttribute('src', 'images/Bubble nebula.jpg');
-    }
-}
-let myButton = document.querySelector('button');
-let myHead = document.querySelector('h1');
 
 function setUserName() {
     let myName = prompt('请输入你的名字。');
